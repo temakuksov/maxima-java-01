a4f7ed5cc1e2a3445c8c848206a3f58f2bedc4ee
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-01-24 02:04:46 +0500

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        String str = "Hello !!!";
+
         System.out.println( "Hello World!" );
+        System.out.println(str);
     }
 }
