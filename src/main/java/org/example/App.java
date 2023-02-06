600e67f6540409a5761f4aad191ccf070d3edd13
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-02-06 22:21:30 +0500

    1 - 4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,8 @@ package org.example;
 
 import java.sql.SQLOutput;
 
-public class App
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         int a = 111;
         int b = 222;
         System.out.println("Было:");
@@ -15,19 +14,49 @@ public class App
         a = a - b;
         System.out.println("Стало:");
         System.out.println("a=" + a + " b=" + b);
-        System.out.println("-------------------------------------");
+        System.out.println("----------< Последовательность Фибоначчи >----------------------");
 
-        byte m=32;
-        System.out.println(m+"-й член последовательности Фибоначчи: "+fibonazzi(m));
+        byte m = 17;   // задать номер члена последовательности Фибоначчи, не более 47, который нужно опеределить
+        System.out.println(m + "-й член последовательности Фибоначчи: " + fibonazzi(m));
+        System.out.println("----------< Является ли число простым >-------------------------");
+        int s = 991;  // вести число для определения простое оно или нет
+        if (s>1) {
+            int r = isSimple(s);
+            if (r == 0) {
+                System.out.println("Число " + s + " - простое число");
+            } else {
+                System.out.println("Число " + s + " делится на " + r);
+            }
+        }
+        if (s<=1) {
+            System.out.println("Для числа " + s + " нельзя определить простое оно или нет!");
+        }
+
     }
-        public static long fibonazzi(byte n){
+
+    public static long fibonazzi(byte n) {
         if (n <= 1) {
-            return (long)0;
+            return (long) 0;
         } else if (n == 2) {
-            return (long)1;
+            return (long) 1;
         } else {
-            return fibonazzi((byte) (n-2)) + fibonazzi((byte) (n-1));
+            return fibonazzi((byte) (n - 2)) + fibonazzi((byte) (n - 1));
         }
     }
 
+    public static int isSimple(int n) {
+        if (n > 2) {
+            int i = 2;
+            int d = 0;
+            while ((i < n) & (d == 0)) {
+                if (n % i == 0) {
+                    d = i;
+                }
+                i++;
+            }
+            return d;
+        } else {
+            return 2;
+        }
+    }
 }
