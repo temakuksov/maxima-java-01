56e48fad3fafe06d052c5c9843241818ac4802b6
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-02-07 01:28:21 +0500

    1 - 5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,23 +14,31 @@ public class App {
         a = a - b;
         System.out.println("Стало:");
         System.out.println("a=" + a + " b=" + b);
-        System.out.println("----------< Последовательность Фибоначчи >----------------------");
+        System.out.println("\n*-------< Последовательность Фибоначчи >--------------------------------------------------");
 
         byte m = 17;   // задать номер члена последовательности Фибоначчи, не более 47, который нужно опеределить
         System.out.println(m + "-й член последовательности Фибоначчи: " + fibonazzi(m));
-        System.out.println("----------< Является ли число простым >-------------------------");
-        int s = 991;  // вести число для определения простое оно или нет
+        System.out.println("\n*-------< Является ли число простым >-----------------------------------------------------");
+        int s = 1001;  // вести число для определения простое оно или нет
         if (s>1) {
             int r = isSimple(s);
             if (r == 0) {
                 System.out.println("Число " + s + " - простое число");
             } else {
-                System.out.println("Число " + s + " делится на " + r);
+                System.out.println("Число " + s + " не является простым и делится на " + r);
             }
         }
         if (s<=1) {
             System.out.println("Для числа " + s + " нельзя определить простое оно или нет!");
         }
+
+        System.out.println("\n*----< Наибольший индекс элемента массива, имеющего максимальную сумму цифр >-------------");
+        int[] mas = {19239,245592,6891,12345,11111111,999,817181,376127,234188,12321}; // ввод массива
+        System.out.println("массив из "+mas.length+ " элементов:");
+        for (int k: mas){
+            System.out.print(k+"  ");
+        }
+        System.out.println("\nНаибольший индекс элемента с макс. суммой цифр: "+maxDigitsSumPosition(mas));
 
     }
 
@@ -59,4 +67,26 @@ public class App {
             return 2;
         }
     }
+
+    public static int sumDigit(int num){
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = (int) num / 10;
+        }
+        return sum;
+    }
+
+    public static byte maxDigitsSumPosition(int[] arr){
+        int max=sumDigit(arr[0]);
+        byte i_max=0;
+        for (byte i=1; i<arr.length; i++) {
+            if (sumDigit(arr[i])>=max) {
+                max=sumDigit(arr[i]);
+                i_max=i;
+            }
+        }
+        return i_max;
+        }
+
 }
